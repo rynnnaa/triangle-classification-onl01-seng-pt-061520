fdadb1a0477bea69cb337b5d07686bd4bdbd189e
Ryna Truong <rynatruong@outlook.com>
2020-07-17 19:04:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,8 +13,8 @@ class Triangle
         raise TriangleError
         puts error.message
         end
-  
-  elsif side_one == side_two && side_two == side_three
+        
+        elsif side_one == side_two && side_two == side_three
       :equilateral
       elsif side_one == side_two || side_two == side_three || side_three == side_two && side_one == side_three
       :isosceles
