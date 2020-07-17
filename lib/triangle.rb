0723cc6643f2c55c94a4c9560321c9f9f82e7d95
Ryna Truong <rynatruong@outlook.com>
2020-07-17 18:53:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+  
+  def Triangle
+    if side_one == side_two && side_three
+      return "equilateral"
+    end
+
+  end
 end
