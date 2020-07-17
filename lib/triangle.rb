a298fae1b31eb9e13a20294bc4cb4ea08795cd4f
Ryna Truong <rynatruong@outlook.com>
2020-07-17 19:12:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,32 @@
 class Triangle
-  attr_accessor :side_one, :side_two, :side_three
-  
-  def initialize(side_one, side_two, side_three)
-    @side_one = side_one
-    @side_two = side_two
-    @side_three = side_three
+
+  attr_accessor :length_one, :length_two, :length_three
+  # write code here
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
   end
-  
+
   class TriangleError < StandardError
-    
+  end
+
   def kind
-    if(side_one * length_two * side_three) == 0 || (side_one + side_two) <= side_three || (side_two + side_three) <= side_one || (side_three + side_one) <= side_two
+    if(length_one * length_two * length_three) == 0 || (length_one + length_two) <= length_three || (length_two + length_three) <= length_one || (length_three + length_one) <= length_two
       begin
         raise TriangleError
         puts error.message
       end
    
-    elsif side_one == side_two && side_two == side_three
+    elsif length_one == length_two && length_two == length_three
       :equilateral
-    elsif side_one == side_two || side_one == side_three || side_two == side_three
+    elsif length_one == length_two || length_one == length_three || length_two == length_three
       :isosceles
     else
       :scalene
     end
   end
 
+  class TriangleError < StandardError
   end
 end
